b4ea7558f174e5d7f24764fc534a415781f8659a
rahelaqambari <rahelaqambari@gmail.com>
2026-04-08 21:33:22 +0430

    changes

--- database/migrations/2026_04_08_170255_create_productdetails_table.php
+++ database/migrations/2026_04_08_170255_create_productdetails_table.php
@@ -11,9 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('productdetails', function (Blueprint $table) {
             $table->id();
-            $table->stirng("name");
             $table->timestamps();
         });
     }
@@ -23,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('productdetails');
     }
 };
