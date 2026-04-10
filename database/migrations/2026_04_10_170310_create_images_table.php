07003d2ef343ed49d17310d55a8e4b91cf7457f8
rahelaqambari <rahelaqambari@gmail.com>
2026-04-10 21:33:40 +0430

    migration model factory changes

--- database/migrations/2026_04_10_170310_create_images_table.php
+++ database/migrations/2026_04_10_170310_create_images_table.php
@@ -11,11 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('reviews', function (Blueprint $table) {
+        Schema::create('images', function (Blueprint $table) {
             $table->id();
-            $table->decimal("rating",3,2);
-            $table->foreignId("pro_id")->constrained("products");
-            $table->foreignId("user_id")->constrained("users");
             $table->timestamps();
         });
     }
@@ -25,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('reviews');
+        Schema::dropIfExists('images');
     }
 };
