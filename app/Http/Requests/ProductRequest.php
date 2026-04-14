826868b1137c030b469e48985a78933bea73a2cc
rahelaqambari <rahelaqambari@gmail.com>
2026-04-14 20:39:42 +0430

    rule

---
+++ app/Http/Requests/ProductRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class ProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "name"=> ["required","string","min:3", Rule::unique('products','name')],
+            "price"=>"required|numeric|min:12|max:15000",
+            "stock"=>"required|integer|min:1|max:200",
+            "brand"=>"required|string",
+            "description"=>"required|string|min:10",
+            "category"=>"required|string",
+        ];
+    }
+}
