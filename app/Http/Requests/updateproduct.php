53332c4e027fba666c77565d6f4379f65729b878
rahelaqambari <rahelaqambari@gmail.com>
2026-04-17 20:49:03 +0430

    rules

--- app/Http/Requests/updateproduct.php
+++ app/Http/Requests/updateproduct.php
@@ -11,7 +11,7 @@ class updateproduct extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,15 @@ class updateproduct extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            "name"=> "nullabel|string|min:3",
+            "stock"=> "nullabel|integer|min:",
+            "price"=> "nullabel|decimal|min:20",
+            "category"=> "nullabel|string|min:3",
+            "description"=> "nullabel|string|min:10",
+            "brand"=> "nullabel|string|min:3",
+            "image_url"=> "nullabel|string",
+            "imageable_type"=> "required|string",
+            "imageable_id"=> "required|integer",
         ];
     }
 }
